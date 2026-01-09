f04d2d756357fb32a71985dac478e2009bda7d14
Aditya Vikram <adityavikram2016@gmail.com>
2026-01-09 14:20:49 -0500

    main layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,22 +5,59 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI.ApplicationSettings;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
 namespace Shinobu
 {
-    /// <summary>
-    /// An empty window that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            this.ExtendsContentIntoTitleBar = true;
+            this.SetTitleBar(titleBar);
+        }
+
+        private void NavView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.IsSettingsInvoked)
+            {
+                //navFrame.Navigate(typeof(SettingsPage));
+            }
+            else if (args.InvokedItemContainer is NavigationViewItem item)
+            {
+                string? tag = item.Tag as string;
+                switch (tag)
+                {
+                    case "library":
+                        //navFrame.Navigate(typeof(LibraryPage));
+                        break;
+                    case "settings":
+                        //navFrame.Navigate(typeof(SettingsPage));
+                        break;
+                    case "words":
+                        //navFrame.Navigate(typeof(WordsPage));
+                        break;
+                }
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender, NavigationViewBackRequestedEventArgs args)
+        {
+            if (navFrame.CanGoBack)
+            {
+                navFrame.GoBack();
+            }
         }
     }
 }
